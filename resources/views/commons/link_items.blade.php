13ef6b1271ac61c851a259af02098c73e77ace90
miyu.fujii <miyu-fujii@dac.co.jp>
2023-05-29 07:00:50 +0000

    誰がログインしているかわかるようにした

--- resources/views/commons/link_items.blade.php
+++ resources/views/commons/link_items.blade.php
@@ -1,4 +1,6 @@
 @if (Auth::check())
+    {{-- 誰がログインしているかわかるように --}}
+    <li><a class="link link-hover" href="{{ route('tasks.index') }}">{{ Auth::user()->name }} の tasklist</a></li>
     {{-- ログアウトへのリンク --}}
     <li><a class="link link-hover" href="#" onclick="event.preventDefault();this.closest('form').submit();">Logout</a></li>
 @else
