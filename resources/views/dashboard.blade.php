687f3ff0badfe300cb23b77c3b86b5498d98a3ac
miyu.fujii <miyu-fujii@dac.co.jp>
2023-05-29 05:41:14 +0000

    見た目の変更など

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
+    @if (Auth::check())
+        <div class="sm:grid sm:grid-cols-3 sm:gap-10">
+            <div class="sm:col-span-2">
+                {{-- 投稿一覧 --}}
+                @include('tasks.index')
+            </div>
+        </div>
+    @else
     <div class="prose hero bg-base-200 mx-auto max-w-full rounded">
         <div class="hero-content text-center my-10">
             <div class="max-w-md mb-10">
@@ -10,4 +18,5 @@
             </div>
         </div>
     </div>
+    @endif
 @endsection
