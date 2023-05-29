78f481f46293edac251e23b2402be23cc6c10c31
miyu.fujii <miyu-fujii@dac.co.jp>
2023-05-29 02:21:29 +0000

    ユーザーとタスクの1対多関係の追加

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,17 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
+    /**
+     * このユーザが所有する投稿。（ Taskモデルとの関係を定義）
+     */
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+    
+    public function loadRelationshipCounts()
+    {
+        $this->loadCount('tasks');
+    }
 }
